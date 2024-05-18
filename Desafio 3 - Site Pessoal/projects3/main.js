e4b8f39451f85d76894bdc9c2946949b473de25f
bellvdorr <142070154+bellvdorr@users.noreply.github.com>
2024-05-17 22:08:44 -0300

    linkando as paginas do site

--- Desafio 3 - Site Pessoal/projects3/main.js
+++ Desafio 3 - Site Pessoal/projects3/main.js
@@ -3,8 +3,8 @@
     //Nav Links    
         const linksNav = document.querySelectorAll("[data-link_nav]");
 
-        linksNav[0].href = "http://127.0.0.1:5500/home1/index.html";
-        linksNav[1].href = "http://127.0.0.1:5500/tools2/index.html";
-        linksNav[2].href = "http://127.0.0.1:5500/projects3/index.html";
+        linksNav[0].href = "../home1/index.html";
+        linksNav[1].href = "../tools2/index.html";
+        linksNav[2].href = "index.html";
 
     //
