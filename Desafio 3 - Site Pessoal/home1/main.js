ebe0517bd11c9261a6527c7fa1269d3d5f1464a6
bellvdorr <142070154+bellvdorr@users.noreply.github.com>
2024-05-17 22:22:28 -0300

    linkando as paginas, apos o deploy

--- Desafio 3 - Site Pessoal/home1/main.js
+++ Desafio 3 - Site Pessoal/home1/main.js
@@ -2,9 +2,9 @@
     //Nav Links    
     const linksNav = document.querySelectorAll("[data-link_nav]");
 
-    linksNav[0].href = "index.html";
-    linksNav[1].href = "../tools2/index.html";
-    linksNav[2].href = "../projects3/index.html";
+    linksNav[0].href = "https://bellvdor-home.vercel.app";
+    linksNav[1].href = "https://bellvdor-tools.vercel.app";
+    linksNav[2].href = "https://bellvdor-projects.vercel.app";
 
 //
 
